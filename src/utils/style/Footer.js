6cd13e051ab2a1bbd3a823dba7f79c6ccb738ea5
Aurelien Allenic <aurelien.allenic@gmail.com>
2022-12-19 15:20:41 +0100

    (fix) changed size of footer for phone size

--- src/utils/style/Footer.js
+++ src/utils/style/Footer.js
@@ -11,6 +11,9 @@ grid-template-columns: repeat(3, 1fr);
 z-index: 1;
 width: 180px;
 height: auto;
+@media(max-width: 795px){
+    width: 120px;
+}
 `
 export const Icons = styled.div`
 gap: 30px;
@@ -18,11 +21,17 @@ display: flex;
 align-items: center;
 justify-content: center;
 padding: 15px;
+@media(max-width: 795px){
+    gap: 15px;
+}
 `
 export const LinkIcon = styled.a`
 color: white;
 font-size: 30px;
 &:hover{
     color: #000;
+}
+@media(max-width: 795px){
+    font-size: 20px;
 }
 `
