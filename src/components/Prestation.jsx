9a31f7121b7b7f4c235d80d45ee7d497dda26bc2
Aurelien Allenic <aurelien.allenic@gmail.com>
2023-01-10 20:36:35 +0100

    (fix) corrected some faults in the text

--- src/components/Prestation.jsx
+++ src/components/Prestation.jsx
@@ -15,8 +15,8 @@ function Prestation() {
                     <ContainerPrestation>
                         <CardPrestation>
                             <MainTitleCard>Linconnu vous propose des prestations de natures diverses :</MainTitleCard>
-                            <ContentCard>Qu'il s'agisse de <Important>Scène</Important> ou de <Important>Close-Up</Important> (table en table), il est possible de réaliser une prestation en <Important>Ile-de-France</Important> de préférence.</ContentCard><ContentCard>Si elle a lieu aileurs, merci de le préciser afin d'étudier les modalités de l'évènement.</ContentCard>
-                            <ContentCard>Bar-Mitzvah, Mariage, anniversaire ou toute autre cérémonie, il est possible de <ContactClass><Link to='contact' spy={true} smooth={true} offset={0} duration={500}>réserver une soirée, une journée</Link></ContactClass>.</ContentCard>
+                            <ContentCard>Qu'il s'agisse de <Important>Scène</Important> ou de <Important>Close-Up</Important> (table en table), il est possible de réaliser une prestation en <Important>Ile-de-France</Important> de préférence.</ContentCard><ContentCard>Si elle a lieu ailleurs, merci de le préciser afin d'étudier les modalités de l'évènement.</ContentCard>
+                            <ContentCard>Pour tout type d'évènement, il est possible de <ContactClass><Link to='contact' spy={true} smooth={true} offset={0} duration={500}>réserver une soirée, une journée</Link></ContactClass>.</ContentCard>
                             <PostScriptum>( Veuillez préciser votre nom, prénom, lieu, date et durée de la prestation, les tarifs varieront en fonction de ces critères )</PostScriptum>
                         </CardPrestation>
                     </ContainerPrestation>
